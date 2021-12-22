0b45c5318db0260570559d137014e24cb47d6c6c
Sayhaan <sayhaansiddiqui@gmail.com>
2021-12-22 11:15:51 -0800

    crash fix

--- ClassLibrary1/Client.cs
+++ ClassLibrary1/Client.cs
@@ -21,38 +21,51 @@ namespace ClassLibrary1
         {
             //openMenuKey = Settings.GetValue("Options", "LightningKey", Keys.J);
             int x = Settings.GetValue("Options", "ConorKey", 0);
-            if(x == 0)
+            if (x == 0)
             {
-                //controls = false;
+                controls = false;
                 openMenuKey = Settings.GetValue("Options", "LightningKey", Keys.J);
             }
             else
             {
-                //controls = true;
+                controls = true;
                 LKey = Settings.GetValue("Options", "LightningKey", RDR2.Control.ScriptPadUp);
             }
             Tick += OnTick;
             Interval = 1;
             KeyDown += OnKeyDown;
         }
+        ~Client() {
 
+        }
         private void OnTick(object sender, EventArgs evt)
         {
-            RaycastResult pos = World.CrosshairRaycast(100000, IntersectOptions.Everything);
-            Vector3 pos1 = pos.HitPosition;
-            if (Game.IsControlJustPressed(2, LKey))
+            //RaycastResult pos = World.CrosshairRaycast(100000, IntersectOptions.Everything);
+            //Vector3 pos1 = pos.HitPosition;
+            if (controls && Game.IsControlJustPressed(2, LKey))
             {
-                Function.Call((Hash)0x67943537D179597C, pos1.X, pos1.Y, pos1.Z);
+                RaycastResult pos = World.CrosshairRaycast(1000, IntersectOptions.Everything, Game.Player.Character);
+                Vector3 pos1 = pos.HitPosition;
+                if (pos1 != null)
+                {
+                    Function.Call((Hash)0x67943537D179597C, pos1.X, pos1.Y, pos1.Z);
+                }
             }
         }
         private void OnKeyDown(object sender, KeyEventArgs e)
         {
-            RaycastResult pos = World.CrosshairRaycast(100000, IntersectOptions.Everything);
-            Vector3 pos1 = pos.HitPosition;
+            //RaycastResult pos = World.CrosshairRaycast(100000, IntersectOptions.Map);
+            //Vector3 pos1 = pos.HitPosition;
             if (e.KeyCode == openMenuKey)
             {
-                Function.Call((Hash)0x67943537D179597C, pos1.X, pos1.Y, pos1.Z);
+                RaycastResult pos = World.CrosshairRaycast(1000, IntersectOptions.Everything, Game.Player.Character);
+                Vector3 pos1 = pos.HitPosition;
+                if (pos1 != null)
+                {
+                    Function.Call((Hash)0x67943537D179597C, pos1.X, pos1.Y, pos1.Z);
+                }
             }
         }
+ 
     }
 }
